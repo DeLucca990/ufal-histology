aefe9ba98981bf099f92f70d4b68d6ad229fa53d
DeLucca990 <pedruca2001@gmail.com>
2024-09-13 11:33:33 -0300

    teste 2 analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-injectSpeedInsights();
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics/>
   </StrictMode>,
 )
